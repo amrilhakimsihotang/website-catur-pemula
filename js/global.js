a3a761a95dbe5581cba3bed0d15a9bb33e2915d4
amrilhakimsihotang <68908992+amrilhakimsihotang@users.noreply.github.com>
2020-09-04 10:17:30 +0700

    Add files via upload

--- js/global.js
+++ js/global.js
@@ -1,1 +1,17 @@
+//create data untuk halaman profil
+var nama1=document.createElement("p");
+var nama2=document.createTextNode("Nama: Amril Hakim Sihotang");
+nama1.appendChild(nama2);
+var element = document.getElementById("profilku");
+element.appendChild(nama1);
+var kelas1=document.createElement("p");
+var kelas2=document.createTextNode("Kelas: Kelas: Belajar Dasar Pemrograman Web(DICODING)");
+kelas1.appendChild(kelas2);
+var element = document.getElementById("profilku");
+element.appendChild(kelas1);
+var konten1=document.createElement("p");
+var konten2=document.createTextNode("Jenis konten: Submission");
+konten1.appendChild(konten2);
+var element = document.getElementById("profilku");
+element.appendChild(konten1);
 
